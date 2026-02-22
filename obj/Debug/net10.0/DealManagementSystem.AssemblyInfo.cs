efb33ca7a55d6a7cc5a605a4873e0d6191c5fa7c
chamaxx9zp <chamakapiyumal@gmail.com>
2026-02-22 14:04:11 +0530

    clean the app

--- obj/Debug/net10.0/DealManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/DealManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DealManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3a852e0624c172616f4772f0796272c75690429")]
 [assembly: System.Reflection.AssemblyProductAttribute("DealManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DealManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
