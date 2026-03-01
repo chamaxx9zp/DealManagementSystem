d75181eef462ebe12b8bac41ca252b73c5f5f851
chamaxx9zp <chamakapiyumal@gmail.com>
2026-03-01 23:17:32 +0530

    add API end points 😎

---
+++ Controllers/DealsController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DealManagementSystem.Data;
+using DealManagementSystem.Entities;
+
+namespace DealManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DealsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DealsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var deals = await _context.Deals
+                .Include(d => d.Hotels)
+                .ToListAsync();
+
+            return Ok(deals);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Deal deal)
+        {
+            if (deal.Hotels == null || !deal.Hotels.Any())
+                return BadRequest("A deal must have at least one hotel.");
+
+            _context.Deals.Add(deal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAll), new { id = deal.Id }, deal);
+        }
+    }
+}
