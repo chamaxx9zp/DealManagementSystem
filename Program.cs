fcaafebf09c6b5571792b358a6a9452243142005
chamaxx9zp <chamakapiyumal@gmail.com>
2026-03-01 21:46:18 +0530

    add swagger middleware 🔓

--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using DealManagementSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using FluentValidation.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers()
+    .AddFluentValidation();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.Run();
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.MapControllers();
+
+app.Run();
